4dbf12c59892bd27e43cf511d25a69b09b497421
Laura Abend <laura.l.abend@gmail.com>
2016-10-22 21:07:21 -0500

    add comment textarea and favorite button

--- public/scripts/giphyapi.service.js
+++ public/scripts/giphyapi.service.js
@@ -3,7 +3,7 @@ angular.module('giphyApp')
        .config(function($routeProvider, $locationProvider) {
          $routeProvider.when('/home', {
            templateUrl: 'views/home.html',
-           controller: 'HomeController as home'
+          //  controller: 'HomeController as home'
          }).when('/favorites', {
            templateUrl: 'views/favorites.html'
          });
