d278fd939a789cc79554b84be4cf7d694b74846c
Laura Abend <laura.l.abend@gmail.com>
2016-10-22 22:05:32 -0500

    add server-side router for database

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const path = require('path');
 
+const gifs = require('./routes/gifs');
+
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/gifs', gifs);
 
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'public/views/index.html'));
 });
-app.listen(3000);
+
+app.listen(3000, function (req, res) {
+  console.log('Now listening on port 3000.');
+  console.log('Go to localhost:3000 to see site.');
+  console.log('ctrl+c shuts down server.');
+});
